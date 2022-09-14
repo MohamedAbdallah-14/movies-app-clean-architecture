73c6427943afd218f12b36cd48479b0eb9245328
Mido Berber <midoberber113@gmail.com>
2022-09-15 00:13:08 +0200

    remove api key

--- lib/core/utils/api_utils.dart
+++ lib/core/utils/api_utils.dart
@@ -1,31 +1,30 @@
-const String api_key = "api_key=8f5075af42b17ddc8ffda9abd7fd68fa";
 
 class ApiUrls {
   static String requestImage(String img) =>
       'https://image.tmdb.org/t/p/w500/$img';
 
-  static String popularMovies(int page) => '/movie/popular?$api_key&page=$page';
+  static String popularMovies(int page) => '/movie/popular?&page=$page';
   static String topRatedMovies(int page) =>
-      '/movie/top_rated?$api_key&page=$page';
+      '/movie/top_rated?&page=$page';
   static String upcomingMovies(int page) =>
-      '/movie/upcoming?$api_key&page=$page';
+      '/movie/upcoming?&page=$page';
   static String nowPlaying(int page) =>
-      '/movie/now_playing?$api_key&page=$page';
+      '/movie/now_playing?&page=$page';
   //static String discoverMovies(int page) => '/discover/movie';
-  static String movieGenres() => '/genre/movie/list?$api_key';
+  static String movieGenres() => '/genre/movie/list?';
   static String searchMovies(String searchText) =>
-      '/search/movie?$api_key&query=$searchText';
-  static String movie(int id) => '/movie/$id?$api_key';
+      '/search/movie?&query=$searchText';
+  static String movie(int id) => '/movie/$id?';
   static String movieByGenres(int genreId) =>
-      '/movie/popular?$api_key&with_genres=$genreId';
-  static String movieCredits(int id) => '/movie/$id/credits?$api_key';
-  static String movieVideos(int id) => '/movie/$id/videos?$api_key';
-  static String actorImages(int id) => '/person/$id/images?$api_key';
-  static String personDetails(int id) => '/person/$id?$api_key';
+      '/movie/popular?&with_genres=$genreId';
+  static String movieCredits(int id) => '/movie/$id/credits?';
+  static String movieVideos(int id) => '/movie/$id/videos?';
+  static String actorImages(int id) => '/person/$id/images?';
+  static String personDetails(int id) => '/person/$id?';
 
   // static Function similarMovies = (int id) => '/movie/$id/similar';
   static String trendingMovies(int page) =>
-      '/trending/movie/day?$api_key&page=$page'; // * timeWindow: day or week
+      '/trending/movie/day?&page=$page'; // * timeWindow: day or week
   // static Function trendingPersons =
   //     (String timeWindow) => '/trending/persons/$timeWindow';
   // static Function personDetails = (int id) => '/person/$id';
